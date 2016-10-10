08e1f7c035b512284e10ef965a48ce2cc709c857
tomasshilton <tomas.shilton@ing.austral.edu.ar>
2016-10-10 20:43:12 -0300

    add cliente2

--- rUBERn/src/Cliente/Cliente.java
+++ rUBERn/src/Cliente/Cliente.java
@@ -1,12 +1,32 @@
 package Cliente;
 
+import rUBERn.Viaje;
+import rUBERn.Coordenada;
 
 public class Cliente {
 
     private Viaje viaje;
-    private Coordenadas posicion;
+    private Coordenada posicion;
 
-    public Cliente
+    public Cliente(Coordenada posicion){
+        this.posicion = posicion;
+    }
 
+    public Viaje getViaje() {
+        return viaje;
+    }
 
+    public Coordenada getPosicion() {
+        return posicion;
+    }
+
+    public void setViaje(Viaje viaje) {
+        this.viaje = viaje;
+    }
+
+    public askForTrip(Coordenada destino){
+        Viaje viaje = new Viaje(posicion, destino);
+        this.viaje = viaje;
+        newTrip(viaje);
+    }
 }
