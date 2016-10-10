662ea0dd5270725fdbceb8ce0026c8463e3cefc1
tomasshilton <tomas.shilton@ing.austral.edu.ar>
2016-10-10 19:13:28 -0300

    test commit

---
+++ rUBERn/src/Cliente/test.java
@@ -0,0 +1,7 @@
+package Cliente;
+
+/**
+ * Created by Tomi on 10/10/2016.
+ */
+public class test {
+}
---
+++ rUBERn/src/rUBERn/test.java
@@ -0,0 +1,7 @@
+package rUBERn;
+
+/**
+ * Created by Tomi on 10/10/2016.
+ */
+public class test {
+}
