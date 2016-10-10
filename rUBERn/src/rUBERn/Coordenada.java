a5472c878838bccbbc768201a7da443d5abf8700
Simon <simon.clasen@ing.austral.edu.ar>
2016-10-10 20:20:04 -0300

    sdvazsd

---
+++ rUBERn/src/rUBERn/Coordenada.java
@@ -0,0 +1,23 @@
+package rUBERn;
+
+/**
+ * Created by Simon on 10/10/2016.
+ */
+public class Coordenada {
+
+    private double x;
+    private double y;
+
+    public Coordenada(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
