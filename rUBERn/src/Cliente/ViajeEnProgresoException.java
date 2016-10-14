233b2d4e414053ecb95d01cf1161d5ead48306f8
Lucas <lucas.manzanelli@ing.austral.edu.ar>
2016-10-13 23:39:56 -0300

    mas test =)

--- rUBERn/src/Cliente/ViajeEnProgresoException.java
+++ rUBERn/src/Cliente/ViajeEnProgresoException.java
@@ -3,8 +3,8 @@ package Cliente;
 /**
  * Created by Lucas on 12/10/2016.
  */
-public class ViajeEnProgresoException extends RuntimeException {
-    public ViajeEnProgresoException(String error) {
+public class ViajeEnProgresoException extends RuntimeException{
+    public ViajeEnProgresoException(String error){
         super(error);
     }
 }
